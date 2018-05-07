a277bab4d13a60a5116862b592226674d9b6dfee
davidkorea <lyfirst@naver.com>
2018-05-07 16:17:10 +0900

    update

--- coffee_calories.py
+++ coffee_calories.py
@@ -9,6 +9,7 @@ def main():
     data_df = pd.read_csv(DATA_FILE)
     cate_groupby = data_df.groupby('Beverage_category')
     cate_count = cate_groupby['Calories'].count()
+    # groupby 返回特殊数据类型，每一列都会返回，需要分析calories，需要选择这一列
     cal_mean = cate_groupby['Calories'].mean()
     print(cate_count, cal_mean)
     cate_count.plot(kind='bar')

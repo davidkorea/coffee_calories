04f9d656989206d6e38a3859d02f95d70a497b8c
davidkorea <lyfirst@naver.com>
2018-05-08 10:33:58 +0900

    add utils

--- coffee_calories.py
+++ coffee_calories.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+from pd_utils import utils
 
 DATA_FILE = './data_pd/coffee_menu.csv'
 # print(os.path.exists(DATA_FILE))
 
 def main():
     data_df = pd.read_csv(DATA_FILE)
+    utils(DATA_FILE)
     cate_groupby = data_df.groupby('Beverage_category')
     cate_count = cate_groupby['Calories'].count()
     # groupby 返回特殊数据类型，每一列都会返回，需要分析calories，需要选择这一列
